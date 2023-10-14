42eeac60974dd031b47242eeb272c7b1ab8ea764
DuaamirXa <140272650+DuaamirXa@users.noreply.github.com>
2023-10-14 20:59:33 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+// chapter#01:-
+// Exercise 1.1 
+console.log("Duaa Mirza");
+//  Exercise 1.3
+
+console.log("Hello World!");
+
+//  Exercise 1.4
+// let a = 10; 
+// console.log("single line comment, not logged");
+
+/* I'm a multi-line comment. Whatever is between the slash asterisk and
+the asterisk slash will not get executed.
+console.log("I'm not logged, because I'm a comment");
+*/
+  
+
+
+// Chapter project
+console.log("Duaa Mirza");
+/* I'm a multi-line comment. Whatever is between the slash asterisk and
+the asterisk slash will not get executed.
+console.log("I'm not logged, because I'm a comment");
+*/
+
+
